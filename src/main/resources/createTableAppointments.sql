184396b65ed00a317273cae1f79a4371064b658a
xnumad <34810600+xnumad@users.noreply.github.com>
2022-01-05 15:03:25 +0100

    DB: Reference user by username

--- src/main/resources/createTableAppointments.sql
+++ src/main/resources/createTableAppointments.sql
@@ -1,6 +1,6 @@
 CREATE TABLE "appointments" (
 	"id"	INTEGER,
-	"user"	INTEGER,
+	"user"	TEXT,
 	"doctor"	INTEGER,
 	"healthProblem"	INTEGER,
 	"healthProblemDescription"	TEXT,
@@ -8,6 +8,6 @@ CREATE TABLE "appointments" (
 	"minutesBeforeReminder"	INTEGER CHECK("minutesBeforeReminder" IN (10, 60, 4320, 10080)),
 	"duration"	INTEGER,
     PRIMARY KEY("id" AUTOINCREMENT),
-	FOREIGN KEY("user") REFERENCES "users"("id") ON UPDATE CASCADE ON DELETE CASCADE,
+	FOREIGN KEY("user") REFERENCES "users"("username") ON UPDATE CASCADE ON DELETE CASCADE,
 	FOREIGN KEY("doctor") REFERENCES "doctors"("id") ON UPDATE CASCADE ON DELETE CASCADE
 );
