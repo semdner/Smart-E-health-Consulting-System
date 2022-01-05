ec1e49b0fb98628903b959fbad992a2488e3e498
xnumad <34810600+xnumad@users.noreply.github.com>
2022-01-05 15:15:52 +0100

    Create Appointment.java

---
+++ src/main/java/Appointment.java
@@ -0,0 +1,62 @@
+import java.sql.SQLException;
+
+public class Appointment {
+    private int id, doctor, healthProblem, timestamp, minutesBeforeReminder, duration;
+    private String user, healthProblemDescription;
+
+    /**
+     * Creates a new appointment object representing a row in the database
+     * @param id
+     * @param user
+     * @param doctor
+     * @param healthProblem
+     * @param healthProblemDescription
+     * @param timestamp
+     * @param minutesBeforeReminder
+     * @param duration
+     */
+    public Appointment(boolean insertIntoDb, int id, String user, int doctor, int healthProblem, String healthProblemDescription, int timestamp, int minutesBeforeReminder, int duration) throws SQLException {
+        this.id = id;
+        this.user = user;
+        this.doctor = doctor;
+        this.healthProblem = healthProblem;
+        this.healthProblemDescription = healthProblemDescription;
+        this.timestamp = timestamp;
+        this.minutesBeforeReminder = minutesBeforeReminder;
+        this.duration = duration;
+        if (insertIntoDb)
+            insertIntoDb();
+    }
+
+    /**
+     * Inserts this appointment into the database.
+     * Only to be used if a new appointment is created.
+     * @throws SQLException
+     */
+    private void insertIntoDb() throws SQLException {
+        Object[][] parameters = {
+                {"id", id},
+                {"user", user},
+                {"doctor", doctor},
+                {"healthProblem", healthProblem},
+                {"healthProblemDescription", healthProblemDescription},
+                {"timestamp", timestamp},
+                {"minutesBeforeReminder", minutesBeforeReminder},
+                {"duration", duration},
+        };
+        DB.insert("appointments", parameters);
+    }
+
+    /**
+     * Updates the entry for this appointment in the database
+     * @param newValues
+     * @throws SQLException
+     */
+    private void update(Object[][] newValues) throws SQLException {
+        DB.update(
+                "appointments",
+                newValues,
+                new Object[][]{{"id", id}}
+        );
+    }
+}
