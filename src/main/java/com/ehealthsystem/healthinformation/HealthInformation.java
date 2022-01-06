44895d81ea8067ccbee01209b88cc9d580ec2ec1
Max Semdner <mxprogramming@protonmail.com>
2022-01-06 12:50:55 +0100

    Add get Methods for health information
    getter methods added for health information.

--- src/main/java/com/ehealthsystem/healthinformation/HealthInformation.java
+++ src/main/java/com/ehealthsystem/healthinformation/HealthInformation.java
@@ -2,22 +2,30 @@ package com.ehealthsystem.healthinformation;
 
 public class HealthInformation {
 
-    private String preExistingConditions, allergies, pastTreatments, currentTreatments, medications;
+    private String preExistingConditions, allergies, medications;
 
 
     /**
      *
      * @param preExistingConditions
      * @param allergies
-     * @param pastTreatments
-     * @param currentTreatments
      * @param medications
      */
-    public HealthInformation(String preExistingConditions, String allergies, String pastTreatments, String currentTreatments, String medications) {
+    public HealthInformation(String preExistingConditions, String allergies, String medications) {
         this.preExistingConditions = preExistingConditions;
         this.allergies = allergies;
-        this.pastTreatments = pastTreatments;
-        this.currentTreatments = currentTreatments;
         this.medications = medications;
+    }
+
+    String getPreExistingConditions() {
+        return preExistingConditions;
+    }
+
+    String getAllergies() {
+        return allergies;
+    }
+
+    String getMedications() {
+        return medications;
     }
 }
