10821954680019775a93c9d327f582a2928847b0
Max Semdner <mxprogramming@protonmail.com>
2022-01-06 10:52:36 +0100

    Add EventHandler for new Appointment
    Added the Eventhandler to the 'make Appointment' Buttons in the Primary Controller, to switch the Scene.

--- src/main/java/com/ehealthsystem/primary/PrimaryController.java
+++ src/main/java/com/ehealthsystem/primary/PrimaryController.java
@@ -1,5 +1,28 @@
 package com.ehealthsystem.primary;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class PrimaryController {
+
+    @FXML
+    public Button makeAppointmentButton1;
+    public Button makeAppointmentButton2;
+
+    public void handleMakeAppointmentButton(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/com/ehealthsystem/appoibtment/appointment1-view.fxml"));
+        Stage stage = (Stage)makeAppointmentButton1.getScene().getWindow();
+        Scene primaryScene = new Scene(root, 1000, 600);
+        stage.setTitle("E-Health System");
+        stage.setScene(primaryScene);
+        stage.show();
+    }
 
 }
