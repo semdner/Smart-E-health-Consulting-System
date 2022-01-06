c5b619d808e4e58b1f6d8e25fbd632bbdf58a744
Max Semdner <mxprogramming@protonmail.com>
2022-01-06 12:46:36 +0100

    Add Constructor to HealthInformation class

--- src/main/java/com/ehealthsystem/healthinformation/HealthInformation.java
+++ src/main/java/com/ehealthsystem/healthinformation/HealthInformation.java
@@ -1,4 +1,23 @@
 package com.ehealthsystem.healthinformation;
 
 public class HealthInformation {
+
+    private String preExistingConditions, allergies, pastTreatments, currentTreatments, medications;
+
+
+    /**
+     *
+     * @param preExistingConditions
+     * @param allergies
+     * @param pastTreatments
+     * @param currentTreatments
+     * @param medications
+     */
+    public HealthInformation(String preExistingConditions, String allergies, String pastTreatments, String currentTreatments, String medications) {
+        this.preExistingConditions = preExistingConditions;
+        this.allergies = allergies;
+        this.pastTreatments = pastTreatments;
+        this.currentTreatments = currentTreatments;
+        this.medications = medications;
+    }
 }
