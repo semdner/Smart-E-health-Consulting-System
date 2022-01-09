00a727066dd5837478999fdeefa9d7f1290f297a
Max Semdner <mxprogramming@protonmail.com>
2022-01-09 11:57:34 +0100

    removed previous database scheme

--- src/main/resources/com/ehealthsystem/database/insertIntoSpecializations.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO specializations VALUES
-(1, "General Practitioner"),
-(2, "Vaccination Center"),
-(3, "Dentist"),
-(4, "Orthodontist"),
-(5, "Oral Surgeon"),
-(6, "Eye Doctor"),
-(7, "Cardiologist"),
-(8, "Sports Physician");
