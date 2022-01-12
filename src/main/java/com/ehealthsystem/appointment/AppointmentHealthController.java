996b04af66f7b5fbc6b828465a68c8c93319c7b4
Max Semdner <mxprogramming@protonmail.com>
2022-01-12 20:13:47 +0100

    add eventhandlers for select all and unselect all

--- src/main/java/com/ehealthsystem/appointment/AppointmentHealthController.java
+++ src/main/java/com/ehealthsystem/appointment/AppointmentHealthController.java
@@ -3,30 +3,25 @@ package com.ehealthsystem.appointment;
 import com.ehealthsystem.database.Database;
 import com.ehealthsystem.healthinformation.HealthInformation;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.chart.PieChart;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.Region;
 import javafx.stage.Stage;
-import org.w3c.dom.Text;
 
 import java.io.IOException;
 import java.net.URL;
-import java.sql.Array;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.ResourceBundle;
-
-import static com.ehealthsystem.appointment.AppointmentUserController.email;
 
 public class AppointmentHealthController implements Initializable {
 
@@ -39,7 +34,13 @@ public class AppointmentHealthController implements Initializable {
     @FXML
     Button backButton;
 
+    @FXML
+    Button selectAllButton;
+
     static String email;
+    private ArrayList<HealthInformation> selectedHealthInformation = new ArrayList<>();
+    private ArrayList<HealthInformation> allHealthInformation = new ArrayList<>();
+    private ArrayList<CheckBox> numberOfCheckBoxes = new ArrayList<>();
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
@@ -69,17 +70,18 @@ public class AppointmentHealthController implements Initializable {
     }
 
     private void loadHealthInformation() throws SQLException {
-        ArrayList<HealthInformation> healthInformation = Database.getHealthInformation(email);
-        fillHealthTable(healthInformation);
+        allHealthInformation = Database.getHealthInformation(email);
+        fillHealthTable(allHealthInformation);
     }
 
-    private void fillHealthTable(ArrayList<HealthInformation> healthInformation) {
-        for(int i = 0; i<healthInformation.size(); i++) {
-            Label ICD = new Label(healthInformation.get(i).getICD());
-            Label health_problem  = new Label(healthInformation.get(i).getDisease());
-            Label medication = new Label(healthInformation.get(i).getMedication());
+    private void fillHealthTable(ArrayList<HealthInformation> allHealthInformation) {
+        for(int i = 0; i<allHealthInformation.size(); i++) {
+            Label ICD = new Label(allHealthInformation.get(i).getICD());
+            Label health_problem  = new Label(allHealthInformation.get(i).getDisease());
+            Label medication = new Label(allHealthInformation.get(i).getMedication());
             CheckBox selection = new CheckBox();
             setStyle(ICD, health_problem, medication, selection);
+            handleCheckBox(selection, allHealthInformation.get(i), i);
 
             healthGridPane.add(ICD, 0, i+1);
             healthGridPane.add(health_problem, 1, i+1);
@@ -95,12 +97,43 @@ public class AppointmentHealthController implements Initializable {
         selection.setFocusTraversable(false);
     }
 
-    public void handleSelectAllButton() {
+    private void handleCheckBox(CheckBox selection, HealthInformation selectedRow, int row) {
+        numberOfCheckBoxes.add(selection);
+        selection.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                if(selection.isSelected()) {
+                    selectedHealthInformation.add(selectedRow);
+                } else {
+                    selectedHealthInformation.remove(selectedRow);
+                }
+            }
+        });
+    }
 
+    public void handleSelectAllButton(ActionEvent event) {
+        for (int i = 0; i < numberOfCheckBoxes.size(); i++) {
+            if(numberOfCheckBoxes.get(i).isSelected()) {
+                continue;
+            } else {
+                numberOfCheckBoxes.get(i).setSelected(true);
+                selectedHealthInformation.add(allHealthInformation.get(i));
+            }
+        }
+    }
+
+    public void handleDeselectAllButton(ActionEvent event) {
+        for (int i = 0; i < numberOfCheckBoxes.size(); i++) {
+            if(numberOfCheckBoxes.get(i).isSelected()) {
+                numberOfCheckBoxes.get(i).setSelected(false);
+                selectedHealthInformation.remove(allHealthInformation.get(i));
+            } else {
+                continue;
+            }
+        }
     }
 
     public static void setEmail(String loginEmail) {
         email = loginEmail;
     }
-
 }
