80b1f709de490df59b32c33f26692d8dc3ea12e9
Max Semdner <mxprogramming@protonmail.com>
2022-01-13 17:06:18 +0100

    update appointment found view and controller

--- src/main/java/com/ehealthsystem/appointment/AppointmentFoundController.java
+++ src/main/java/com/ehealthsystem/appointment/AppointmentFoundController.java
@@ -1,4 +1,26 @@
 package com.ehealthsystem.appointment;
 
-public class AppointmentFoundController {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class AppointmentFoundController implements Initializable {
+
+    @FXML
+    ComboBox reminderComboBox;
+
+    static Appointment newAppointment;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        String[] choices = {"10 minutes", "1 hour", "3 days", "1 week"};
+        reminderComboBox.getItems().addAll(choices);
+    }
+
+    public static void setAppointment(Appointment passedAppointment) {
+        newAppointment = passedAppointment;
+    }
 }
