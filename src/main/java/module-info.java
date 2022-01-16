506abe26414e062a1f9267ac470c420cbc957f18
Max Semdner <mxprogramming@protonmail.com>
2022-01-16 18:28:09 +0100

    update dependencies and module-info

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,17 +1,21 @@
 module Smart.E.health.Consulting.System {
-
     requires javafx.graphics;
     requires javafx.controls;
+    requires javafx.web;
     requires javafx.fxml;
     requires java.sql;
     requires jbcrypt;
     requires com.google.common;
+    requires google.maps.services;
+    requires com.google.gson;
     requires java.mail;
+    requires org.json;
 
     opens com.ehealthsystem.appointment to javafx.fxml;
     opens com.ehealthsystem.login to javafx.fxml;
     opens com.ehealthsystem.primary to javafx.fxml;
     opens com.ehealthsystem.registration to javafx.fxml;
+    opens com.ehealthsystem.doctor to javafx.fxml;
 
     exports com.ehealthsystem;
 }
