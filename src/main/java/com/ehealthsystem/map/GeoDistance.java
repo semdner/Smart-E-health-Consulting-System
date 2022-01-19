c371ab327322bd846876b56f8f95979af94118c0
xnumad <34810600+xnumad@users.noreply.github.com>
2022-01-19 14:56:43 +0100

    Fix distance errors (GeoDistance)
    Don't serialize the `DistanceMatrix` to JSON. Instead, it is more reliable to work with the Java objects (e.g. to avoid exceptions like "JSON key not found").
    
    Additionally: Removing " km" from the string is highly unreliable! `distance.humanReadable` is not always given in kilometers (e.g. if distance is < 100 meters).
    Luckily, an exception is thrown (`NumberFormatException` by `Double.parseDouble()`) in that case; otherwise, this edge case would be unnoticed during runtime.

--- src/main/java/com/ehealthsystem/map/GeoDistance.java
+++ src/main/java/com/ehealthsystem/map/GeoDistance.java
@@ -1,12 +1,9 @@
 package com.ehealthsystem.map;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 import com.google.maps.DistanceMatrixApi;
 import com.google.maps.GeoApiContext;
 import com.google.maps.errors.ApiException;
 import com.google.maps.model.DistanceMatrix;
-import org.json.JSONObject;
 
 import java.io.IOException;
 import java.sql.SQLException;
@@ -21,21 +18,6 @@ public class GeoDistance {
 
         // make API request for distance matrix
         DistanceMatrix request = DistanceMatrixApi.getDistanceMatrix(context, user, doctor).await();
-        return getDistanceFromJSON(request);
-    }
-
-    private static double getDistanceFromJSON(DistanceMatrix request) {
-        // Create JSON object from Distance Matrix
-        Gson gson = new GsonBuilder().setPrettyPrinting().create();
-        String resultsJson = gson.toJson(request);
-        JSONObject json = new JSONObject(resultsJson);
-
-        // Get Distance value from JSON object
-        String resultDistance = json.getJSONArray("rows").getJSONObject(0).getJSONArray("elements").getJSONObject(0).getJSONObject("distance").getString("humanReadable");
-
-        // remove parts of String for casting
-        resultDistance = resultDistance.replace(" km", "");
-        return Double.parseDouble(resultDistance);
-
+        return (double)request.rows[0].elements[0].distance.inMeters/1000;
     }
 }
