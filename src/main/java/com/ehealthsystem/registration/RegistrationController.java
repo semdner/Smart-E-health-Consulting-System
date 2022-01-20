f9ef0d9bb107918cb633d34d301fcc076dd8361b
xnumad <34810600+xnumad@users.noreply.github.com>
2022-01-20 19:42:54 +0100

    Optimize imports

--- src/main/java/com/ehealthsystem/registration/RegistrationController.java
+++ src/main/java/com/ehealthsystem/registration/RegistrationController.java
@@ -1,6 +1,5 @@
 package com.ehealthsystem.registration;
 
-import com.ehealthsystem.login.LoginController;
 import com.ehealthsystem.tools.BirthdayCheck;
 import com.ehealthsystem.tools.EmailCheck;
 import com.ehealthsystem.tools.SceneSwitch;
@@ -9,22 +8,14 @@ import com.ehealthsystem.user.User;
 import javafx.event.ActionEvent;
 import javafx.event.Event;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Node;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.*;
-
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
-import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.net.URL;
 import java.sql.SQLException;
-import java.time.LocalDate;
-import java.time.Period;
 import java.util.ResourceBundle;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
