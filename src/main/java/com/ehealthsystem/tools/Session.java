bfe48d2b5d69f505c8f8055ea9ec980b111db69c
xnumad <34810600+xnumad@users.noreply.github.com>
2022-01-20 20:29:27 +0100

    Add Appointment to Session

--- src/main/java/com/ehealthsystem/tools/Session.java
+++ src/main/java/com/ehealthsystem/tools/Session.java
@@ -1,12 +1,14 @@
 package com.ehealthsystem.tools;
 
+import com.ehealthsystem.appointment.Appointment;
 import com.ehealthsystem.database.Database;
 import com.ehealthsystem.user.User;
 
 import java.sql.SQLException;
 
 public class Session {
-    public static User user;
+    public static User user; //user that is currently logged in
+    public static Appointment appointment; //an appointment that is currently created
 
     public static void loginUser(String email) throws SQLException {
         user = Database.getUserFromEmail(email);
