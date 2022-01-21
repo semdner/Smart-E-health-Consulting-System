8e366493cd7e7ced57ac1a55c4bb48e90af97711
xnumad <34810600+xnumad@users.noreply.github.com>
2022-01-21 10:40:34 +0100

    Optimize imports

--- src/main/java/com/ehealthsystem/appointment/AppointmentInformationController.java
+++ src/main/java/com/ehealthsystem/appointment/AppointmentInformationController.java
@@ -5,13 +5,8 @@ import com.ehealthsystem.tools.SceneSwitch;
 import com.ehealthsystem.tools.Session;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Node;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.*;
-import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.net.URL;
