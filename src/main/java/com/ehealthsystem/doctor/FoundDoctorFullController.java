18ed0cac396052d077668644cf6d5e59a817597d
Max Semdner <mxprogramming@protonmail.com>
2022-01-22 17:27:45 +0100

    load appointments when scene is switched

--- src/main/java/com/ehealthsystem/doctor/FoundDoctorFullController.java
+++ src/main/java/com/ehealthsystem/doctor/FoundDoctorFullController.java
@@ -1,13 +1,47 @@
 package com.ehealthsystem.doctor;
 
+import com.ehealthsystem.appointment.AppointmentInCreation;
+import com.ehealthsystem.database.Database;
+import com.ehealthsystem.healthinformation.HealthInformation;
 import com.ehealthsystem.map.DoctorDistance;
+import com.ehealthsystem.tools.SceneSwitch;
+import com.ehealthsystem.tools.Session;
 import com.google.maps.errors.ApiException;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import javafx.scene.web.WebView;
 
 import java.io.IOException;
+import java.sql.SQLException;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 
 public class FoundDoctorFullController {
+
+    @FXML
+    Label doctorLabel;
+
+    @FXML
+    Label addressLabel;
+
+    @FXML
+    Label specializationLabel;
+
+    @FXML
+    Label dateLabel;
+
+    @FXML
+    GridPane scheduleGridPane;
 
     @FXML
     private WebView mapWebView;
@@ -15,11 +49,14 @@ public class FoundDoctorFullController {
     private DoctorDistance doctor = new DoctorDistance();
     private String userGeoData;
     private String doctorGeoData;
+    private ArrayList<Label> timeLabelList = new ArrayList<>();
 
-    public void start() throws IOException, InterruptedException, ApiException {
+    public void start() throws IOException, InterruptedException, ApiException, SQLException {
         loadPage(userGeoData, doctorGeoData);
         loadDoctorData();
+        loadSchedule();
     }
+
 
     public void setDoctor(DoctorDistance doctor) {
         this.doctor = doctor;
@@ -33,11 +70,60 @@ public class FoundDoctorFullController {
         this.doctorGeoData = doctorGeoData;
     }
 
-    private void loadDoctorData() {
+    private void loadPage(String userGeoData, String doctorGeoData) {
 
     }
 
-    private void loadPage(String userGeoData, String doctorGeoData) {
+    private void loadDoctorData() {
+        doctorLabel.setText("Dr. " + doctor.getDoctor().getFirstName() + " " + doctor.getDoctor().getLastName());
+        addressLabel.setText(doctorGeoData);
+    }
 
+    private void loadSchedule() throws SQLException {
+        ArrayList<DoctorAppointment> doctorAppointmentList = Database.loadDoctorAppointments(doctor.getDoctor().getFirstName(), doctor.getDoctor().getLastName(), Session.appointment.getDate());
+        dateLabel.setText(Session.appointment.getDate().toString());
+        int column = 0;
+        int row = 1;
+        for(int i = 0; i<doctorAppointmentList.size(); i++) {
+            Label time = new Label(doctorAppointmentList.get(i).getTime().toString());
+            Button timeButton = new Button();
+            handleTimeButton(time, timeButton);
+            setStyle(time, timeButton);
+            if(i % 2 == 0 && i != 0) {
+                column = 0;
+                row++;
+            }
+            scheduleGridPane.add(time, column, row);
+            scheduleGridPane.add(timeButton, column, row);
+            column++;
+        }
+    }
+
+    private void handleTimeButton(Label time, Button timeButton) {
+        timeLabelList.add(time);
+        String timeStr = time.getText();
+        timeButton.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                for (int i = 0; i<timeLabelList.size(); i++) {
+                    timeLabelList.get(i).setTextFill(Color.web("#000000"));
+                }
+                DateTimeFormatter TimeFormatter = DateTimeFormatter.ofPattern("H:mm");
+                LocalTime setTime = LocalTime.parse(timeStr, TimeFormatter);
+                Session.appointment.setTime(setTime);
+                time.setTextFill(Color.web("#FF0000"));
+                System.out.println(Session.appointment.getTime());
+            }
+        });
+    }
+
+    private void setStyle(Label time, Button timeButton) {
+        time.setStyle("-fx-font-size: 15px;");
+        timeButton.setStyle("-fx-opacity: 0%");
+        timeButton.setPrefWidth(100);
+    }
+
+    public void handleBackButton(ActionEvent event) throws IOException {
+        SceneSwitch.switchTo(event, "appointment/appointmentFound-view.fxml", "Make appointment");;
     }
 }
