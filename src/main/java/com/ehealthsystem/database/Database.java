b458b0e9f8e0b3c7dae4762c46e53588f483704e
xnumad <34810600+xnumad@users.noreply.github.com>
2022-01-30 22:35:04 +0100

    Fix appointment ordering
    Noticed because of the mention in 1866d949548f8703a718915c495aecc2fa5e39a1. Appointments *were* sorted by date (ascending), but then *descending by time* (i.e. if you had multiple appointments for a day, the last one was shown first), which is fixed now.
    This only concerns the result set as returned from the database, not the UI display yet.

--- src/main/java/com/ehealthsystem/database/Database.java
+++ src/main/java/com/ehealthsystem/database/Database.java
@@ -570,7 +570,7 @@ public class Database {
      * @return usersAppointments
      */
     public static ArrayList<Appointment> getUsersAppointments(String username) throws SQLException {
-        String query = "SELECT * FROM appointment WHERE user = ? ORDER BY date, time DESC"; //ordering for display as list
+        String query = "SELECT * FROM appointment WHERE user = ? ORDER BY date, time"; //ordering for display as list
         PreparedStatement statement = connection.prepareStatement(query);
         statement.setString(1, username);
         ResultSet rs = statement.executeQuery();
