754beef453754a17719b273cc5b566ac101d269f
xnumad <34810600+xnumad@users.noreply.github.com>
2022-01-31 16:11:26 +0100

    Remove comma afterwards
    *Slightly* better readability

--- src/main/java/com/ehealthsystem/tools/StringEnumerator.java
+++ src/main/java/com/ehealthsystem/tools/StringEnumerator.java
@@ -6,12 +6,8 @@ public class StringEnumerator {
     public static String enumerate(ArrayList<String> list) {
         StringBuilder specializations = new StringBuilder();
         for (int i = 0; i < list.size(); i++) {
-            if (list.size()-1 == i) {
-                specializations.append(list.get(i));
-            } else {
-                specializations.append(list.get(i)).append(", ");
-            }
+            specializations.append(list.get(i)).append(", ");
         }
-        return specializations.toString();
+        return specializations.substring(0, specializations.length() - ", ".length());
     }
 }
