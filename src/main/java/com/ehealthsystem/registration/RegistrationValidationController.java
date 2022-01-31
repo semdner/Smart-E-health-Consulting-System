90a4855399c50ec8fe21388a902280787020a862
CodeRed98 <seegerdarius@yahoo.de>
2022-01-31 15:32:00 +0100

    Revert "Merge branch 'main' of https://github.com/mxcom/Smart-E-health-Consulting-System"
    This reverts commit 7586be2bfb9a27bb7920523fe8d0b2991219fff3, reversing
    changes made to 6d270d773716e044697d86426789bebd317bef21.

--- src/main/java/com/ehealthsystem/registration/RegistrationValidationController.java
+++ src/main/java/com/ehealthsystem/registration/RegistrationValidationController.java
@@ -14,7 +14,7 @@ import java.sql.SQLException;
 
 public class RegistrationValidationController {
 
-    static String validation;
+    static String validation2;
 
     @FXML
     TextField codeTextField;
@@ -26,7 +26,7 @@ public class RegistrationValidationController {
     String password;
 
     public void start(User user, String password) {
-        System.out.println(validation);
+        System.out.println(validation2);
         this.password = password;
         newUser = user;
     }
@@ -34,7 +34,21 @@ public class RegistrationValidationController {
     public void handleRegistrationButton(ActionEvent event) throws SQLException, IOException {
         if(validateCode()) {
             Session.user = newUser;
-            newUser.insertIntoDb(password);
+            Object[][] parameters = {
+                    {"username", Session.user.getUsername()},
+                    {"email", Session.user.getMail()},
+                    {"first_name", Session.user.getFirstName()},
+                    {"last_name", Session.user.getLastName()},
+                    {"street", Session.user.getStreet()},
+                    {"number", Session.user.getHouseNo()},
+                    {"zip", Session.user.getZipCode()},
+                    {"birthday", Session.user.getBirthDate()},
+                    {"sex", Session.user.getGender()},
+                    {"password", Database.hashPassword(password)},
+                    {"insurance_name", Session.user.getInsuranceName()},
+                    {"private_insurance", Session.user.isPrivateInsurance()},
+            };
+            Database.insert("user", parameters);
             SceneSwitch.switchToCentered(event, "primary/primary-view.fxml", "E-Health System");
         } else {
             errorLabel.setText("Invalid code");
@@ -43,7 +57,7 @@ public class RegistrationValidationController {
     }
 
     public boolean validateCode() {
-        if(codeTextField.getText().equals(validation)) {
+        if(codeTextField.getText().equals(validation2)) {
             return true;
         } else {
             return false;
@@ -51,7 +65,7 @@ public class RegistrationValidationController {
     }
 
     public static void setValidation(String validation) {
-        RegistrationValidationController.validation = validation;
+        validation2 = validation;
     }
 
 }
