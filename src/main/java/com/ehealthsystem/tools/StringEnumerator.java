78265504722d9021f9697c06502131b8a21dcf38
xnumad <34810600+xnumad@users.noreply.github.com>
2022-01-31 16:51:00 +0100

    Use enhanced for loop

--- src/main/java/com/ehealthsystem/tools/StringEnumerator.java
+++ src/main/java/com/ehealthsystem/tools/StringEnumerator.java
@@ -5,8 +5,8 @@ import java.util.ArrayList;
 public class StringEnumerator {
     public static String enumerate(ArrayList<String> list) {
         StringBuilder specializations = new StringBuilder();
-        for (int i = 0; i < list.size(); i++) {
-            specializations.append(list.get(i)).append(", ");
+        for (String s : list) {
+            specializations.append(s).append(", ");
         }
         return specializations.substring(0, specializations.length() - ", ".length());
     }
