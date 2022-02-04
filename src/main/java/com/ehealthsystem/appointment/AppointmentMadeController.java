cbf548c92f2f2ee039c3582303944c9f95c86b4e
xnumad <34810600+xnumad@users.noreply.github.com>
2022-02-04 17:50:36 +0100

    Don't show cancel and edit appointment buttons for past appointments

--- src/main/java/com/ehealthsystem/appointment/AppointmentMadeController.java
+++ src/main/java/com/ehealthsystem/appointment/AppointmentMadeController.java
@@ -15,6 +15,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
@@ -22,6 +23,7 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.sql.SQLException;
+import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class AppointmentMadeController {
@@ -47,12 +49,22 @@ public class AppointmentMadeController {
     @FXML
     WebView mapWebView;
 
+    @FXML
+    Button editAppointmentButton;
+
+    @FXML
+    Button cancelAppointmentButton;
+
     Appointment loadedAppointment;
     String doctorGeoData;
 
     public void start(Appointment appointment) throws SQLException, IOException, InterruptedException, ApiException {
         loadedAppointment = appointment;
         loadAppointment();
+        if (loadedAppointment.getDate().isBefore(LocalDate.now())) { //past appointment
+            editAppointmentButton.setVisible(false);
+            cancelAppointmentButton.setVisible(false);
+        }
         loadDoctor();
         loadGMap();
     }
