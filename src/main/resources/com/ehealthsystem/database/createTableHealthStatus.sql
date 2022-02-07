edf3227676e5b0fb71efa4ef1267c35ef9079db3
xnumad <34810600+xnumad@users.noreply.github.com>
2022-02-07 14:23:41 +0100

    Fix user deletion not working when they had health statuses

--- src/main/resources/com/ehealthsystem/database/createTableHealthStatus.sql
+++ src/main/resources/com/ehealthsystem/database/createTableHealthStatus.sql
@@ -5,6 +5,6 @@ CREATE TABLE health_status
     ICD                 INTEGER,
     PRIMARY KEY("id" AUTOINCREMENT),
     UNIQUE (user_id, ICD),
-    FOREIGN KEY (user_id) REFERENCES user(user_id),
+    FOREIGN KEY (user_id) REFERENCES user(user_id) ON UPDATE CASCADE ON DELETE CASCADE,
     FOREIGN KEY (ICD) REFERENCES disease(ICD)
 );
