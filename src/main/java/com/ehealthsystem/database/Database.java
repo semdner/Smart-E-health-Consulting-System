2a1c167494fa534bee216bd66afc6ca206d4ac21
mxcom <56640416+mxcom@users.noreply.github.com>
2022-02-08 16:11:15 +0100

    admin deletion option

--- src/main/java/com/ehealthsystem/database/Database.java
+++ src/main/java/com/ehealthsystem/database/Database.java
@@ -790,9 +790,8 @@ public class Database {
             user_id = rs.getInt("user_id");
         }
 
-        String queryDeleteion = "DELETE FROM user WHERE user_id = ?;";
+        String queryDeleteion = "DELETE FROM appointment WHERE user = '" + username + "';";
         statement = connection.prepareStatement(queryDeleteion);
-        statement.setInt(1, user_id);
         statement.executeUpdate();
 
         queryDeleteion = "DELETE FROM health_status WHERE user_id = ?;";
@@ -800,8 +799,9 @@ public class Database {
         statement.setInt(1, user_id);
         statement.executeUpdate();
 
-        queryDeleteion = "DELETE FROM appointment WHERE user = '" + username + "';";
+        queryDeleteion = "DELETE FROM user WHERE user_id = ?;";
         statement = connection.prepareStatement(queryDeleteion);
+        statement.setInt(1, user_id);
         statement.executeUpdate();
     }
 }
