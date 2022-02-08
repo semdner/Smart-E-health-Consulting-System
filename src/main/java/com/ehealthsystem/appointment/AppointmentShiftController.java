0771348cad0130a7a54501b55acf5aac0d9d7bce
CodeRed98 <seegerdarius@yahoo.de>
2022-02-08 10:48:03 +0100

    Send Mail to doctor when appointment shifted

--- src/main/java/com/ehealthsystem/appointment/AppointmentShiftController.java
+++ src/main/java/com/ehealthsystem/appointment/AppointmentShiftController.java
@@ -143,6 +143,16 @@ public class AppointmentShiftController extends ScheduleLoader {
                 ),
                 "This is to confirm that your appointment with the doctor %s was shifted.".formatted(loadedAppointment.getDoctor().getLastName()),false
         );
+        SendEmail.sendMail(Session.user.getMail(),
+                "Appointment shift confirmation: Patient: %s: %s -> %s %s".formatted(
+                Session.user.getFirstName(),
+                Session.user.getLastName(),
+                loadedAppointment.getDate().isEqual(oldDateTime.toLocalDate()) ? "" : loadedAppointment.getDate().format(Session.dateFormatter), //don't display date twice if only the time was changed
+                loadedAppointment.getTime().format(Session.timeFormatter)
+        ),
+                "This is to confirm that your appointment with the patient %s %s was shifted.".formatted(
+                        Session.user.getFirstName(),
+                        Session.user.getLastName()), true);
         SceneSwitch.switchTo(event,"primary/primary-view.fxml", "E-Health-System");
     }
 
