17cac6b885852e4959718516aa562710935ac526
Max Semdner <mxprogramming@protonmail.com>
2022-02-08 06:18:03 +0100

    add java doc

--- src/main/java/com/ehealthsystem/healthInformation/HealthInformationTableView.java
+++ src/main/java/com/ehealthsystem/healthInformation/HealthInformationTableView.java
@@ -2,26 +2,50 @@ package com.ehealthsystem.healthInformation;
 
 import javafx.beans.property.SimpleStringProperty;
 
+/**
+ * Health information in a form that they are storeable as an observable
+ */
 public class HealthInformationTableView {
     private SimpleStringProperty ICD, healthProblem;
 
+    /**
+     * set ICD and health problem when creating an object
+     * @param ICD
+     * @param healthProblem
+     */
     public HealthInformationTableView(String ICD, String healthProblem) {
         this.ICD = new SimpleStringProperty(ICD);
         this.healthProblem = new SimpleStringProperty(healthProblem);
     }
 
+    /**
+     * set the ICD
+     * @param ICD
+     */
     public void setICD(String ICD) {
         this.ICD.set(ICD);
     }
 
+    /**
+     * set the health problem
+     * @param healthProblem
+     */
     public void setHealthProblem(String healthProblem) {
         this.healthProblem.set(healthProblem);
     }
 
+    /**
+     * return the ICD
+     * @return
+     */
     public String getICD() {
         return ICD.get();
     }
 
+    /**
+     * return the health problem
+     * @return
+     */
     public String getHealthProblem() {
         return healthProblem.get();
     }
