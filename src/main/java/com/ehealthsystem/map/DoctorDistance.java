10c1642be19f41e3ec6916bd6c178bf9f6b78276
mxcom <56640416+mxcom@users.noreply.github.com>
2022-02-08 08:15:44 +0100

    add java doc

--- src/main/java/com/ehealthsystem/map/DoctorDistance.java
+++ src/main/java/com/ehealthsystem/map/DoctorDistance.java
@@ -7,24 +7,45 @@ public class DoctorDistance {
     String geoData;
     Doctor doctor;
 
+    /**
+     * Needed for the FoundDoctorController the doctor doesn't have any values at the beginning assigned
+     */
     public DoctorDistance() {
 
     }
 
+    /**
+     * set the set distance from the doctor to the user, the geodata and the indication of which doctor is meant
+     * @param distance distance from doctor to user
+     * @param geodata geo data of the doctor
+     * @param doctor doctor object which indicates which doctor
+     */
     public DoctorDistance(double distance, String geodata, Doctor doctor) {
         this.distance = distance;
         this.geoData = geodata;
         this.doctor = doctor;
     }
 
+    /**
+     * get the distance from doctor to user
+     * @return
+     */
     public double getDistance() {
         return distance;
     }
 
+    /**
+     * get the doctors geodata
+     * @return
+     */
     public String getGeoData() {
         return geoData;
     }
 
+    /**
+     * get the doctor
+     * @return
+     */
     public Doctor getDoctor() {
         return doctor;
     }
