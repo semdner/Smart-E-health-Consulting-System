20f35529175e6aab94caf181c5e19a9a112d2359
xnumad <34810600+xnumad@users.noreply.github.com>
2022-02-09 14:08:51 +0100

    Display health problem name before the description

--- src/main/java/com/ehealthsystem/doctor/FoundDoctorFullController.java
+++ src/main/java/com/ehealthsystem/doctor/FoundDoctorFullController.java
@@ -250,9 +250,9 @@ public class FoundDoctorFullController extends ScheduleLoader {
                 "You successfully made an appointment with the doctor %s on %s at %s. Their address is %s.".formatted(appointment.getDoctor().getLastName(), date, time, doctorGeoData)
                         + (reminderChoice.getMinutes() != 0 ? " A reminder email will be sent to you %s ahead.".formatted(reminderChoice.getLabel()) : ""),false
         );
-        SendEmail.sendMail(Session.user.getMail(),"Patient Health Information","Here is the health problem of your patient: %s,  %s".formatted(
-                appointment.getHealthProblemDescription(),
-                Session.appointment.getHealthProblemChoice()
+        SendEmail.sendMail(Session.user.getMail(),"Patient Health Information","Here is the health problem of your patient: %s, %s".formatted(
+                Session.appointment.getHealthProblemChoice(),
+                appointment.getHealthProblemDescription()
         ),true);
         SceneSwitch.switchTo(event, "primary/primary-view.fxml", "E-Health System");
     }
